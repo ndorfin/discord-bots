30dde68960db8745d7706b547a3a8c8af48f2602
Shaun O'Connell <ndorfin@gmail.com>
2024-04-11 15:47:48 +1200

    Copy the Player Trueskill and Match History scripts for CSV output

---
+++ scripts/csv_match_history.py
@@ -0,0 +1,84 @@
+from discord_bots.models import FinishedGame, FinishedGamePlayer, Player, Session
+
+session = Session()
+
+finished_games: list[FinishedGame] = (
+    session.query(FinishedGame)
+    .order_by(FinishedGame.finished_at.desc())
+    .limit(200)
+    .all()
+)
+
+with open('./csv/match_history.csv', 'w') as file_object:
+    print(
+        (
+            "timestamp,"
+            "winning_team,"
+            "team0_win%,"
+            "team1_win%,"
+            "is_upset,"
+            "t0player0,"
+            "t0player1,"
+            "t0player2,"
+            "t0player3,"
+            "t0player4,"
+            "t1player0,"
+            "t1player1,"
+            "t1player2,"
+            "t1player3,"
+            "t1player4"
+        ),
+        file=file_object
+    )
+    for finished_game in finished_games:
+        winning_team = ""
+        if finished_game.winning_team == -1:
+            winning_team = "tie"
+        elif finished_game.winning_team == 0:
+            winning_team = "be"
+        elif finished_game.winning_team == 1:
+            winning_team = "ds"
+        team0_players: list[Player] = (
+            session.query(Player)
+            .join(
+                FinishedGamePlayer,
+                Player.id == FinishedGamePlayer.player_id,
+            )
+            .filter(
+                FinishedGamePlayer.finished_game_id == finished_game.id,
+                FinishedGamePlayer.team == 0,
+            )
+        )
+        team1_players: list[Player] = (
+            session.query(Player)
+            .join(
+                FinishedGamePlayer,
+                Player.id == FinishedGamePlayer.player_id,
+            )
+            .filter(
+                FinishedGamePlayer.finished_game_id == finished_game.id,
+                FinishedGamePlayer.team == 1,
+            )
+        )
+
+        team0_ids: str = ",".join([str(player.id) for player in team0_players])
+        team1_ids: str = ",".join([str(player.id) for player in team1_players])
+
+        is_upset = False
+        if finished_game.win_probability > 0.5 and finished_game.winning_team == 1:
+            is_upset = True
+        if finished_game.win_probability < 0.5 and finished_game.winning_team == 0:
+            is_upset = True
+
+        print(
+            (
+                f"{finished_game.finished_at},"
+                f"{winning_team},"
+                f"{round(finished_game.win_probability,2)},"
+                f"{round(1-finished_game.win_probability,2)},"
+                f"{is_upset},"
+                f"{team0_ids},"
+                f"{team1_ids}"
+            ),
+            file=file_object
+        )
---
+++ scripts/csv_players.py
@@ -0,0 +1,46 @@
+from discord_bots.models import FinishedGame, FinishedGamePlayer, Player, Session
+
+session = Session()
+total_games = 0
+players = session.query(Player).order_by(Player.rated_trueskill_mu.desc()).all()
+
+with open('./csv/players.csv', 'w') as file_object:
+    print(
+        (
+            "id,"
+            "name,"
+            "games,"
+            "rated_ts_mu,"
+            "rated_ts_sigma,"
+            "last_activity_at"
+        ),
+        file=file_object
+    )
+
+    for i, player in enumerate(players):
+        total_games = 0
+        wins = 0
+        losses = 0
+        ties = 0
+        finished_game: FinishedGame
+        finished_games = (
+            session.query(FinishedGame)
+            .join(
+                FinishedGamePlayer, FinishedGame.id == FinishedGamePlayer.finished_game_id
+            )
+            .filter(FinishedGamePlayer.player_id == player.id)
+            .all()
+        )
+        total_games += len(finished_games)
+
+        print(
+            (
+                f"{player.id},"
+                f"{player.name},"
+                f"{total_games},"
+                f"{player.rated_trueskill_mu},"
+                f"{player.rated_trueskill_sigma},"
+                f"{player.last_activity_at}"
+            ),
+            file=file_object
+        )
